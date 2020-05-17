7e215a768b65937729227000831a7768005eda8b
Frederik Runge-Dalager <au601914@uni.au.dk>
2020-05-17 17:03:20 +0200

    login virker

--- modelsApp/src/components/login.vue
+++ modelsApp/src/components/login.vue
@@ -1,91 +1,72 @@
 <template>
-    <div class="md-layout-item">
-        <form @submit.prevent="loginfunction">
-            <md-card>
-                <md-ripple>
-                    <md-card-header>
-                        <div class="md-title">Login</div>
-                    </md-card-header>
-
-                    <md-card-content>
-                       
-                        <md-field md-clearable class="userNameField">
-                            <label>UserName</label>
-                            <md-input v-model="UserName"></md-input>
-                            <md-option value="center">Center</md-option>
-                        </md-field>
-
-                        <md-field class="passwordField">
-                            <label>Password</label>
-                            <md-input v-model="Password" type="password"></md-input>
-                            <md-option value="center">Center</md-option>
-                        </md-field>
-                        
-                    </md-card-content>
-
-                    <md-card-actions>
-                        <md-button type="submit">Login</md-button>
-                    </md-card-actions>
-
-
-                </md-ripple>
-            </md-card>
-        </form>
+  <div class="container" style="margin-top:20px;">
+    <div class="card">
+      <div class="container">
+        <input v-model="username" class="input" type="text" placeholder="Email">
+      </div>
+      <div class="container">
+        <input v-model="password" class="input" type="password" placeholder="Password">
+      </div>
+      <div class="container">
+        <button @click="login" style="margin:5px; margin-bottom:10px">Login</button>
+        <p>{{statusMsg}}</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import router from "../Router";
-
-    export default {
-        name: 'login',
-        data: () => ({
-            UserName: 'UserName',
-            PassWord: 'Password'
-        }),
-
-        methods:
-                {
-                    loginfunction() {
-                        
-                        var url = 'https://localhost:44368/api/Account/login';
-                        var data = {
-                            email: this.UserName,
-                            Password: this.PassWord,
-                            OldPassword: this.PassWord
-                        };
-
-                        fetch(url, {
-                            method: 'POST',
-                            body: JSON.stringify(data),
-                            headers: new Headers({ 'Content-Type': 'application/json' })
-                        }).then(res => res.json()).then((token) => {
-                            localStorage.setItem("token", token.jwt);
-                            router.push("/")
-                        }).catch(error => alert("Error!!! " + error))
-                    }
-                }
-    };
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        statusMsg: '', //Remove
+        modelId: '' // Remove
+      }
+    },
+    methods: {
+      login() {
+        fetch('https://localhost:44368/api/Account/login', {
+          method: 'POST',
+          body: JSON.stringify({
+            email: this.username,
+            password: this.password
+          }),
+          headers: new Headers({
+            'Content-Type': 'application/json'
+          })
+        }).then(res => res.json())
+          .then((token) => {
+            localStorage.setItem("token", token.jwt);
+            if (token.jwt == undefined)
+              this.statusMsg = 'Unsucessful';
+            else {
+              this.statusMsg = 'Sucessful';
+            }
+          })
+          .catch(error => console.error('Error:', error));
+      }
+    }
+  }
 </script>
 
+
 <style scoped>
-    
-    .passwordField
-    {
-        position: relative;
+
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 500px;
+    border-radius: 5px;
+    text-align: center;
+  }
+    /* On mouse-over, add a deeper shadow */
+    .card:hover {
+      box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
 
-    .userNameField
-    {
-        position: relative;
-    }
-
-    .md-layout-item 
-    {
-        position: relative;
-        height: 400px;
-        width: 400px;
-          
-    }
+  .input {
+    margin: 5px;
+  }
 </style>
-
